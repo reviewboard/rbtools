28b1e575f4f7472dbbc18d9fcc3e2f4f0bc89f70
chipx86 <chipx86@5efc13c4-1f27-0410-8691-ff2d1f55687e>
2009-03-27 08:43:26 +0000

    Move post-review into a new top-level "rbtools" package
    Since the dawn of time, post-review has lived in the Review Board tree, despite being a client script and not necessarily being needed on the server.
    
    This moves post-review into a new rbtools package, which users will be able to easy_install (easy_install -U RBTools). The simplejson dependency will be fetched, and users won't have to go hunting in SVN for a script.
    
    Over time, I'd like to add new scripts into here (such as one to apply a diff from a review request), and further down the road I'd like to create an "rbapi" (or something) package that contains a Python API for talking to a Review Board server, which rbtools (and post-review) will then use. For now, the goal is to just get post-review moved over.
    
    Reviewed at http://reviews.review-board.org/r/788/
    
    
    git-svn-id: https://reviewboard.googlecode.com/svn/trunk/rbtools@1861 5efc13c4-1f27-0410-8691-ff2d1f55687e

---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#
+# setup.py -- Installation for rbtools.
+#
+# Copyright (C) 2009 Christian Hammond
+# Copyright (C) 2009 David Trowbridge
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+from setuptools.command.test import test
+
+
+VERSION = "0.2beta1"
+IS_RELEASE = True
+
+
+if IS_RELEASE:
+    download_url = "http://downloads.review-board.org/releases/"
+else:
+    download_url = "http://downloads.review-board.org/nightlies/"
+
+
+setup(name="RBTools",
+      version=VERSION,
+      license="MIT",
+      description="Command line applications for use with Review Board",
+      scripts=[
+          "scripts/post-review",
+      ],
+      install_requires=['simplejson'],
+      dependency_links = [
+          download_url,
+      ],
+      include_package_data=True,
+      maintainer="Christian Hammond",
+      maintainer_email="chipx86@chipx86.com",
+      url="http://www.review-board.org/",
+      download_url=download_url,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Software Development",
+      ]
+)
