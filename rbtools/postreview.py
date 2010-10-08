17bd34542311e83f7468ac9c75296060eb130df9
David Trowbridge <trowbrds@gmail.com>
2010-10-07 21:23:13 -0700

    Don't use the login cookie if username/password or submit-as are provided.
    If a user is giving submit-as or a username and password on the command line,
    it's unlikely that they want to log in using the session stored in the cookie.
    This change moves the cookie check until after we've looked at the command-line
    arguments. This is based loosely on http://reviews.reviewboard.org/r/713/ but is
    a different implementation.
    
    Fixes bug 762.

--- rbtools/postreview.py
+++ rbtools/postreview.py
@@ -318,9 +318,6 @@ class ReviewBoardServer(object):
         Logs in to a Review Board server, prompting the user for login
         information if needed.
         """
-        if not force and self.has_valid_cookie():
-            return
-
         if (options.diff_filename == '-' and
             not options.username and not options.submit_as and
             not options.password):
@@ -333,6 +330,10 @@ class ReviewBoardServer(object):
             username = options.username
         elif options.submit_as:
             username = options.submit_as
+        elif not force and has_valid_cookie():
+            # We delay the check for a valid cookie until after looking at args,
+            # so that it doesn't override the command line.
+            return
         else:
             username = raw_input('Username: ')
 
