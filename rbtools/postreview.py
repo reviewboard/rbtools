91c4019f2a934d06eebf170e639bb3719159184b
Peter Ward <peteraward@gmail.com>
2011-08-16 00:04:33 -0700

    Accept a single revision ID for --revision-range for Mercurial.
    Mercurial now supports --revision-range with a single revision ID now. If
    given a single revision, it will find the first parent of that revision and
    use that as the first revision of the diff.
    
    Reviewed at http://reviews.reviewboard.org/r/2546/

--- rbtools/postreview.py
+++ rbtools/postreview.py
@@ -2674,7 +2674,17 @@ class MercurialClient(SCMClient):
         if self._type != 'hg':
             raise NotImplementedError
 
-        r1, r2 = revision_range.split(':')
+        if ':' in revision_range:
+            r1, r2 = revision_range.split(':')
+        else:
+            # If only 1 revision is given, we find the first parent and use
+            # that as the second revision.
+            #
+            # We could also use "hg diff -c r1", but then we couldn't reuse the
+            # code for extracting descriptions.
+            r2 = revision_range
+            r1 = execute(["hg", "parents", "-r", r2,
+                          "--template", "{rev}\n"]).split()[0]
 
         if options.guess_summary and not options.summary:
             options.summary = self.extract_summary(r2)
