982f6edbe2baf9f7f09be7ac950c6d6cb4ed239f
Christian Hammond <chipx86@chipx86.com>
2013-01-26 02:33:55 -0800

    Improve our capabilities support to not hard-code 2 level resolution.
    Our capabilities are no longer limited to a category.name format.
    Capabilities.has_capability can take variable arguments and traverse the
    capabilities list from that. For example, passing 'foo', 'bar', 'foobar'
    will properly navigate {'foo': {'bar': {'foobar': True}}}.

--- rbtools/api/capabilities.py
+++ rbtools/api/capabilities.py
@@ -3,9 +3,17 @@ class Capabilities(object):
     def __init__(self, capabilities):
         self.capabilities = capabilities
 
-    def has_capability(self, category, name):
+    def has_capability(self, *args):
+        caps = self.capabilities
+
         try:
-            return self.capabilities[category][name]
+            for arg in args:
+                caps = caps[arg]
+
+            # If only part of a capability path is specified, we don't want
+            # to evaluate to True just because it has contents. We want to
+            # only say we have a capability if it is indeed 'True'.
+            return caps is True
         except (TypeError, KeyError):
             # The server either doesn't support the capability,
             # or returned no capabilities at all.
---
+++ rbtools/api/tests.py
@@ -0,0 +1,35 @@
+import unittest
+
+from rbtools.api.capabilities import Capabilities
+
+
+class CapabilitiesTests(unittest.TestCase):
+    """Tests for rbtools.api.capabilities.Capabilities"""
+    def test_has_capability(self):
+        """Testing Capabilities.has_capability with supported capability"""
+        caps = Capabilities({
+            'foo': {
+                'bar': {
+                    'value': True,
+                }
+            }
+        })
+
+        self.assertTrue(caps.has_capability('foo', 'bar', 'value'))
+
+    def test_has_capability_with_unknown_capability(self):
+        """Testing Capabilities.has_capability with unknown capability"""
+        caps = Capabilities({})
+        self.assertFalse(caps.has_capability('mycap'))
+
+    def test_has_capability_with_partial_path(self):
+        """Testing Capabilities.has_capability with partial capability path"""
+        caps = Capabilities({
+            'foo': {
+                'bar': {
+                    'value': True,
+                }
+            }
+        })
+
+        self.assertFalse(caps.has_capability('foo', 'bar'))
