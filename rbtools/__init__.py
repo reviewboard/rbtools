7f3397c63f3fc72574d77098cb313091953cbf8f
Christian Hammond <chipx86@chipx86.com>
2014-01-14 18:27:18 -0800

    Release RBTools 0.5.5.

--- rbtools/__init__.py
+++ rbtools/__init__.py
@@ -29,22 +29,25 @@
 #
 # This is in the format of:
 #
-#   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
+#   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 5, 4, 'final', 0, True)
+VERSION = (0, 5, 5, 0, 'final', 0, True)
 
 
 def get_version_string():
     version = '%s.%s' % (VERSION[0], VERSION[1])
 
-    if VERSION[2]:
+    if VERSION[2] or VERSION[3]:
         version += ".%s" % VERSION[2]
 
-    if VERSION[3] != 'final':
-        if VERSION[3] == 'rc':
-            version += ' RC%s' % VERSION[4]
+    if VERSION[3]:
+        version += ".%s" % VERSION[3]
+
+    if VERSION[4] != 'final':
+        if VERSION[4] == 'rc':
+            version += ' RC%s' % VERSION[5]
         else:
-            version += ' %s %s' % (VERSION[3], VERSION[4])
+            version += ' %s %s' % (VERSION[4], VERSION[5])
 
     if not is_release():
         version += " (dev)"
@@ -55,17 +58,20 @@ def get_version_string():
 def get_package_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
 
-    if VERSION[2]:
+    if VERSION[2] or VERSION[3]:
         version += ".%s" % VERSION[2]
 
-    if VERSION[3] != 'final':
-        version += '%s%s' % (VERSION[3], VERSION[4])
+    if VERSION[3]:
+        version += ".%s" % VERSION[3]
+
+    if VERSION[4] != 'final':
+        version += '%s%s' % (VERSION[4], VERSION[5])
 
     return version
 
 
 def is_release():
-    return VERSION[5]
+    return VERSION[6]
 
 
 __version_info__ = VERSION[:-1]
