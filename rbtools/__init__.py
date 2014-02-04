49accb9a918568c073dd083e1e80a98f9268b936
Christian Hammond <chipx86@chipx86.com>
2014-02-03 19:04:28 -0800

    Bump to 0.6 dev.

--- rbtools/__init__.py
+++ rbtools/__init__.py
@@ -31,7 +31,7 @@
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 5, 8, 0, 'alpha', 0, False)
+VERSION = (0, 6, 0, 0, 'alpha', 0, False)
 
 
 def get_version_string():
