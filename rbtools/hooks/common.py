468379e450f845560e74b15a5bd429f8eafeadd1
Anselina Chia <anselina.chia@gmail.com>
2014-03-27 14:00:56 -0700

    Add hooks module, containing utility functions common to hook scripts.
    This is a new 'hooks' module in RBTools. rbtools/hooks/common.py contains
    functions common to all hook scripts, and rbtools/hooks/git.py contains
    functions for Git hook scripts.
    
    Testing Done:
    - Tested the functions individually, and got the expected results.
    - More testing was done in https://reviews.reviewboard.org/r/5403/ for the Git
      post-receive and pre-receive hook scripts that use this new module.
    
    Reviewed at https://reviews.reviewboard.org/r/5543/

---
+++ rbtools/hooks/common.py
@@ -0,0 +1,109 @@
+import logging
+import re
+import subprocess
+
+from rbtools.api.client import RBClient
+from rbtools.api.errors import APIError, ServerInterfaceError
+
+
+SUBMITTED = 'submitted'
+
+
+class HookError(Exception):
+    pass
+
+
+def get_api(server_url, username, password):
+    """Returns an RBClient instance and the associated root resource.
+
+    Hooks should use this method to gain access to the API, instead of
+    instantianting their own client.
+    """
+    api_client = RBClient(server_url, username=username, password=password)
+
+    try:
+        api_root = api_client.get_root()
+    except ServerInterfaceError, e:
+        raise HookError('Could not reach the Review Board server at %s: %s'
+                        % (server_url, e))
+    except APIError, e:
+        raise HookError('Unexpected API Error: %s' % e)
+
+    return api_client, api_root
+
+
+def execute(command):
+    """Executes the specified command and returns the stdout output."""
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    output = process.communicate()[0].strip()
+
+    if process.returncode:
+        logging.warning('Failed to execute command: %s', command)
+        return None
+
+    return output
+
+
+def initialize_logging():
+    """Sets up a log handler to format log messages.
+
+    Warning, error, and critical messages will show the level name as a prefix,
+    followed by the message.
+    """
+    root = logging.getLogger()
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+    handler.setLevel(logging.WARNING)
+    root.addHandler(handler)
+
+
+def get_review_request_id(regex, commit_message):
+    """Returns the review request ID referenced in the commit message.
+
+    We assume there is at most one review request associated with each commit.
+    If a matching review request cannot be found, we return 0.
+    """
+    match = regex.search(commit_message)
+    return (match and int(match.group('id'))) or 0
+
+
+def get_review_request(review_request_id, api_root):
+    """Returns the review request resource for the given ID."""
+    try:
+        review_request = api_root.get_review_request(
+            review_request_id=review_request_id)
+    except APIError, e:
+        raise HookError('Error getting review request: %s' % e)
+
+    return review_request
+
+
+def close_review_request(server_url, username, password, review_request_id,
+                         description):
+    """Closes the specified review request as submitted."""
+    api_client, api_root = get_api(server_url, username, password)
+    review_request = get_review_request(review_request_id, api_root)
+
+    if review_request.status == SUBMITTED:
+        logging.warning('Review request #%s is already %s.',
+                        review_request_id, SUBMITTED)
+        return
+
+    if description:
+        review_request = review_request.update(status=SUBMITTED,
+                                               description=description)
+    else:
+        review_request = review_request.update(status=SUBMITTED)
+
+    print('Review request #%s is set to %s.' %
+          (review_request_id, review_request.status))
+
+
+def get_review_request_approval(server_url, username, password,
+                                review_request_id):
+    """Returns the approval information for the given review request."""
+    api_client, api_root = get_api(server_url, username, password)
+    review_request = get_review_request(review_request_id, api_root)
+
+    return review_request.approved, review_request.approval_failure
---
+++ rbtools/hooks/git.py
@@ -0,0 +1,107 @@
+from collections import defaultdict
+from copy import deepcopy
+
+from rbtools.hooks.common import execute, get_review_request_id
+
+
+def get_branch_name(ref_name):
+    """Returns the branch name corresponding to the specified ref name."""
+    branch_ref_prefix = 'refs/heads/'
+
+    if ref_name.startswith(branch_ref_prefix):
+        return ref_name[len(branch_ref_prefix):]
+
+
+def get_commit_hashes(old_rev, new_rev):
+    """Returns a list of abbreviated commit hashes from old_rev to new_rev."""
+    git_command = ['git', 'rev-list', '--abbrev-commit', '--reverse', '%s..%s'
+                   % (old_rev, new_rev)]
+    return execute(git_command).split('\n')
+
+
+def get_unique_commit_hashes(ref_name, new_rev):
+    """Returns a list of abbreviated commit hashes unique to ref_name."""
+    git_command = ['git', 'rev-list', new_rev, '--abbrev-commit', '--reverse',
+                   '--not']
+    git_command.extend(get_excluded_branches(ref_name))
+    return execute(git_command).strip().split('\n')
+
+
+def get_excluded_branches(ref_name):
+    """Returns a list of all branches, excluding the specified branch."""
+    git_command = ['git', 'for-each-ref', 'refs/heads/', '--format=%(refname)']
+    all_branches = execute(git_command).strip().split('\n')
+    return [branch.strip() for branch in all_branches if branch != ref_name]
+
+
+def get_branches_containing_commit(commit_hash):
+    """Returns a list of all branches containing the specified commit."""
+    git_command = ['git', 'branch', '--contains', commit_hash]
+    branches = execute(git_command).replace('*', '').split('\n')
+    return [branch.strip() for branch in branches]
+
+
+def get_commit_message(commit):
+    """Returns the specified commit's commit message."""
+    git_command = ['git', 'show', '-s', '--pretty=format:%B', commit]
+    return execute(git_command).strip()
+
+
+def get_review_id_to_commits_map(lines, regex):
+    """Returns a dictionary, mapping a review request ID to a list of commits.
+
+    The commits must be in the form: oldrev newrev refname (separated by
+    newlines), as given by a Git pre-receive or post-receive hook.
+
+    If a commit's commit message does not contain a review request ID, we append
+    the commit to the key 0.
+    """
+    review_id_to_commits_map = defaultdict(list)
+
+    # Store a list of new branches (which have an all-zero old_rev value)
+    # created in this push to handle them specially.
+    new_branches = []
+    null_sha1 = '0' * 40
+
+    for line in lines:
+        old_rev, new_rev, ref_name = line.split()
+        branch_name = get_branch_name(ref_name)
+
+        if not branch_name or new_rev == null_sha1:
+            continue
+
+        if old_rev == null_sha1:
+            new_branches.append(branch_name)
+            commit_hashes = get_unique_commit_hashes(ref_name, new_rev)
+        else:
+            commit_hashes = get_commit_hashes(old_rev, new_rev)
+
+        for commit_hash in commit_hashes:
+            if commit_hash:
+                commit_message = get_commit_message(commit_hash)
+                review_request_id = get_review_request_id(regex, commit_message)
+
+                commit = '%s (%s)' % (branch_name, commit_hash)
+                review_id_to_commits_map[review_request_id].append(commit)
+
+    # If there are new branches, check every commit in the dictionary
+    # (corresponding to only old branches) to see if the new branches also
+    # contain that commit.
+    if new_branches:
+        review_id_to_commits_map_copy = deepcopy(review_id_to_commits_map)
+
+        for review_id, commit_list in review_id_to_commits_map_copy.iteritems():
+            for commit in commit_list:
+                commit_branch = commit[:commit.find('(') - 1]
+
+                if commit_branch in new_branches:
+                    continue
+
+                commit_hash = commit[commit.find('(') + 1:-1]
+                commit_branches = get_branches_containing_commit(commit_hash)
+
+                for branch in set(new_branches).intersection(commit_branches):
+                    new_commit = '%s (%s)' % (branch, commit_hash)
+                    review_id_to_commits_map[review_id].append(new_commit)
+
+    return review_id_to_commits_map
