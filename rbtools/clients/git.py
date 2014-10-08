5ac5618bfe42eb762b980f39b885f6e31b70dd45
David Trowbridge <trowbrds@gmail.com>
2014-10-08 10:18:05 -0600

    Fix GIT_USE_EXT_DIFF.
    The code to enable-disable external diff programs for git wasn't quite right,
    in that it worked as intended if the config parameter was not present, but if
    it was, the value was backwards.
    
    Testing done:
    Set my diff viewer to an external program and verified that the setting now
    worked as intended.
    
    Reviewed at https://reviews.reviewboard.org/r/6417/

--- rbtools/clients/git.py
+++ rbtools/clients/git.py
@@ -473,7 +473,7 @@ class GitClient(SCMClient):
         # things from breaking horribly if someone configures a graphical diff
         # viewer like p4merge or kaleidoscope. This can be overridden by
         # setting GIT_USE_EXT_DIFF = True in ~/.reviewboardrc
-        if self.user_config.get('GIT_USE_EXT_DIFF', False):
+        if not self.user_config.get('GIT_USE_EXT_DIFF', False):
             diff_cmd.append('--no-ext-diff')
 
         diff_lines = execute(diff_cmd + files,
