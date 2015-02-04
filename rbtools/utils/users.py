414a694264c5ec6a4f5926ce1a7519613c0d8e9e
Christian Hammond <christian@beanbaginc.com>
2015-02-03 17:44:21 -0800

    Fix more Unicode issues on Windows.
    There were a couple remaining calls to `getpass()` still using Unicode
    strings, and plenty of Unicode entries in environment variables. These
    broke on Windows, since the implementation there expects byte strings.
    
    This changes the last `getpass()` calls and normalizes all environment
    variables on Windows prior to calling `subprocess.Popen()`.
    
    Testing Done:
    Installed a build on Windows. I triggered the authentication paths to
    ensure `getpass()` was called in each case. I also triggered several
    `execute()` paths, all of which worked without blowing up.
    
    Bugs closed: 3729
    
    Reviewed at https://reviews.reviewboard.org/r/6898/

--- rbtools/utils/users.py
+++ rbtools/utils/users.py
@@ -27,7 +27,7 @@ def get_authenticated_session(api_client, api_root, auth_required=False):
                         'server at %s, please login.' % api_client.url)
         sys.stderr.write('Username: ')
         username = input()
-        password = getpass.getpass('Password: ')
+        password = getpass.getpass(b'Password: ')
         api_client.login(username, password)
 
         try:
