e538bc6d3ff015b61307b29aa26bf4a30068bf2b
Christian Hammond <christian@beanbaginc.com>
2015-03-09 16:21:26 -0700

    Bump RBTools master to 0.8.0 dev.

--- rbtools/__init__.py
+++ rbtools/__init__.py
@@ -34,7 +34,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 7, 2, 0, 'final', 0, True)
+VERSION = (0, 8, 0, 0, 'alpha', 0, False)
 
 
 def get_version_string():
