reviewboard/rbtools

commit e20a1ba0e75d839ccf08ab1bc8446b7482f0027d

Improve robustness of --svn-show-copies-as-adds option and respect included files
This fixes Issue #3649 where previously `history_scheduled_with_commit()` did
not respect explicitly included files when determining if an SVN changeset
contained an addition-with-history.  `history_scheduled_with_commit()` now adds
included files to the call to `svn status` similar to how it is done for an SVN
changelist.

In the process of fixing the issue I came across several other minor
deficiencies related to the --svn-show-copies-as-adds option and I have also
cleaned those up:

1. The SVN client version is checked before doing anything related to
   --svn-show-copies-as-adds, as the underlying SVN argument
   (--show-copies-as-adds) did not come into existence until version 1.7.0.

2. The existence of --svn-show-copies-as-adds is checked before calling
   `history_scheduled_with_commit()`.  If the user has already specified this
   option then there is no point in doing additional work.

3. In `history_scheduled_with_commit()` add '-q' argument to `svn status` call
   to suppress unversioned items.  For working copies with unversioned items,
   the addition of this argument will result in fewer status lines to process.

4. `svn status` reports details about files contained in changelists at the end
   of its output, after detailing all non-changelist files.  In the case where
   we are not working with a changelist, return early when we get to the
   changelist "section" of the status output so as not to possibly incorrectly
   detect history from a changelist.

5. In `history_scheduled_with_commit()` respect exclude_patterns so as not to
   possibly incorrectly detect history from an excluded file.

To support checking the SVN client version, `is_valid_version()` is relocated
from rbtools/clients/git.py to rbtools/utils/checks.py.  Moving to this common
location allows the function to be used by both the git and SVN clients without

rbtools/clients/svn.py
@@ -10,9 +10,11 @@ from six.moves.urllib.parse import unquote
 from rbtools.api.errors import APIError
 from rbtools.clients import SCMClient, RepositoryInfo
 from rbtools.clients.errors import (InvalidRevisionSpecError,
-                                    TooManyRevisionsError)
-from rbtools.utils.checks import check_gnu_diff, check_install
-from rbtools.utils.diffs import filter_diff, normalize_patterns
+                                    OptionsCheckError, TooManyRevisionsError)
+from rbtools.utils.checks import (check_gnu_diff, check_install,
+                                  is_valid_version)
+from rbtools.utils.diffs import (filename_match_any_patterns, filter_diff,
+                                 normalize_patterns)
 from rbtools.utils.filesystem import make_empty_files, walk_parents
 from rbtools.utils.process import execute
 
@@ -36,6 +38,8 @@ class SVNClient(SCMClient):
 
     REVISION_WORKING_COPY = '--rbtools-working-copy'
     REVISION_CHANGELIST_PREFIX = '--rbtools-changelist:'
+
+    SHOW_COPIES_AS_ADDS_MIN_VERSION = (1, 7, 0)
 
     def __init__(self, **kwargs):
         super(SVNClient, self).__init__(**kwargs)
@@ -77,6 +81,11 @@ class SVNClient(SCMClient):
         # Now that we know it's SVN, make sure we have GNU diff installed,
         # and error out if we don't.
         check_gnu_diff()
+
+        # Grab version of SVN client and store as a tuple in the form:
+        #   (major_version, minor_version, micro_version)
+        ver_string = self._run_svn(['--version', '-q'], ignore_errors=True)
+        self.subversion_client_version = tuple(map(int, ver_string.split('.')))
 
         return SVNRepositoryInfo(path, base_path, m.group(1))
 
@@ -282,15 +291,20 @@ class SVNClient(SCMClient):
 
         diff_cmd.extend(include_files)
 
-        if self.history_scheduled_with_commit(changelist):
+        if is_valid_version(self.subversion_client_version,
+                            self.SHOW_COPIES_AS_ADDS_MIN_VERSION):
             svn_show_copies_as_adds = getattr(
                 self.options, 'svn_show_copies_as_adds', None)
+
             if svn_show_copies_as_adds is None:
-                sys.stderr.write("One or more files in your changeset has "
-                                 "history scheduled with commit. Please try "
-                                 "again with '--svn-show-copies-as-adds=y/n"
-                                 "'\n")
-                sys.exit(1)
+                if self.history_scheduled_with_commit(changelist,
+                                                      include_files,
+                                                      exclude_patterns):
+                    sys.stderr.write("One or more files in your changeset has "
+                                     "history scheduled with commit. Please "
+                                     "try again with "
+                                     "'--svn-show-copies-as-adds=y/n'.\n")
+                    sys.exit(1)
             else:
                 if svn_show_copies_as_adds in 'Yy':
                     diff_cmd.append("--show-copies-as-adds")
@@ -313,17 +327,37 @@ class SVNClient(SCMClient):
             'diff': ''.join(diff),
         }
 
-    def history_scheduled_with_commit(self, changelist):
+    def history_scheduled_with_commit(self, changelist, include_files,
+                                      exclude_patterns):
         """ Method to find if any file status has '+' in 4th column"""
-        status_cmd = ['status', '--ignore-externals']
+        status_cmd = ['status', '-q', '--ignore-externals']
 
         if changelist:
             status_cmd.extend(['--changelist', changelist])
 
+        if include_files:
+            status_cmd.extend(include_files)
+
         for p in self._run_svn(status_cmd, split_lines=True):
             try:
+                if not changelist and p.startswith('--- Changelist'):
+                    # svn status returns changelist information last.  If we
+                    # hit a line starting with '--- Changelist' then we have
+                    # reached the changelist section and can exit the loop as
+                    # we are not interested in changelists.
+                    break
+
                 if p[3] == '+':
-                    return True
+                    if exclude_patterns:
+                        # We found a file with history, but first we must make
+                        # sure that it is not being excluded.
+                        filename = p[8:].rstrip()
+
+                        if not filename_match_any_patterns(filename,
+                                                           exclude_patterns):
+                            return True
+                    else:
+                        return True
             except IndexError:
                 # This may be some other output, or just doesn't have the
                 # data we're looking for. Move along.
@@ -657,6 +691,15 @@ class SVNClient(SCMClient):
             cmdline += ['--password', self.options.svn_password]
 
         return execute(cmdline, *args, **kwargs)
+
+    def check_options(self):
+        if getattr(self.options, 'svn_show_copies_as_adds', None):
+            if (len(self.options.svn_show_copies_as_adds) > 1
+                or self.options.svn_show_copies_as_adds not in 'YyNn'):
+                raise OptionsCheckError(
+                    'Invalid value \'%s\' for --svn-show-copies-as-adds '
+                    'option. Valid values are \'y\' or \'n\'.' %
+                    self.options.svn_show_copies_as_adds)
 
 
 class SVNRepositoryInfo(RepositoryInfo):

rbtools/clients/tests.py
@@ -10,6 +10,7 @@ from tempfile import mktemp
 from textwrap import dedent
 
 from nose import SkipTest
+from six.moves import cStringIO as StringIO
 
 from rbtools.api.capabilities import Capabilities
 from rbtools.clients import RepositoryInfo
@@ -21,6 +22,7 @@ from rbtools.clients.mercurial import MercurialClient
 from rbtools.clients.perforce import PerforceClient, P4Wrapper
 from rbtools.clients.svn import SVNRepositoryInfo, SVNClient
 from rbtools.tests import OptionsStub
+from rbtools.utils.checks import is_valid_version
 from rbtools.utils.filesystem import load_config_files, make_tempfile
 from rbtools.utils.process import execute
 from rbtools.utils.testbase import RBTestBase
@@ -459,19 +461,6 @@ class GitClientTests(SCMClientTests):
         self.assertTrue('parent_base' not in revisions)
         self.assertEqual(revisions['base'], base_commit_id)
         self.assertEqual(revisions['tip'], tip_commit_id)
-
-    def test_is_valid_version(self):
-        """Testing GitClient.is_valid_version"""
-        self.assertTrue(self.client.is_valid_version((1, 0, 0), (1, 0, 0)))
-        self.assertTrue(self.client.is_valid_version((1, 1, 0), (1, 0, 0)))
-        self.assertTrue(self.client.is_valid_version((1, 0, 1), (1, 0, 0)))
-        self.assertTrue(self.client.is_valid_version((1, 1, 0), (1, 1, 0)))
-        self.assertTrue(self.client.is_valid_version((1, 1, 1), (1, 1, 0)))
-        self.assertTrue(self.client.is_valid_version((1, 1, 1), (1, 1, 1)))
-
-        self.assertFalse(self.client.is_valid_version((0, 9, 9), (1, 0, 0)))
-        self.assertFalse(self.client.is_valid_version((1, 0, 9), (1, 1, 0)))
-        self.assertFalse(self.client.is_valid_version((1, 1, 0), (1, 1, 1)))
 
     def test_get_raw_commit_message(self):
         """Testing GitClient.get_raw_commit_message"""
@@ -1148,6 +1137,7 @@ class SVNClientTests(SCMClientTests):
         os.chdir(os.path.join(self.clone_dir, 'svn-repo'))
 
         self.client = SVNClient(options=self.options)
+        self.options.svn_show_copies_as_adds = None
 
     def _run_svn(self, command):
         return execute(['svn'] + command, env=None, split_lines=False,
@@ -1337,6 +1327,139 @@ class SVNClientTests(SCMClientTests):
         self.assertTrue('diff' in result)
         self.assertEqual(md5(result['diff']).hexdigest(),
                          '1b68063237c584d38a9a3ddbdf1f72a2')
+
+    def test_show_copies_as_adds_enabled(self):
+        """Testing SVNClient with --show-copies-as-adds functionality
+        enabled"""
+        self.check_show_copies_as_adds('y', 'ac1835240ec86ee14ddccf1f2236c442')
+
+    def test_show_copies_as_adds_disabled(self):
+        """Testing SVNClient with --show-copies-as-adds functionality
+        disabled"""
+        self.check_show_copies_as_adds('n', 'd41d8cd98f00b204e9800998ecf8427e')
+
+    def check_show_copies_as_adds(self, state, md5str):
+        """Helper function to evaluate --show-copies as adds"""
+        self.client.get_repository_info()
+
+        # Ensure valid SVN client version.
+        if not is_valid_version(self.client.subversion_client_version,
+                                self.client.SHOW_COPIES_AS_ADDS_MIN_VERSION):
+            raise SkipTest('Subversion client is too old to test '
+                           '--show-copies-as-adds.')
+
+        self.options.svn_show_copies_as_adds = state
+
+        self._svn_add_dir('dir1')
+        self._svn_add_dir('dir2')
+        self._run_svn(['copy', 'foo.txt', 'dir1'])
+
+        # Generate identical diff from checkout root, via changelist, and via
+        # explicit include target.
+
+        revisions = self.client.parse_revision_spec()
+        result = self.client.diff(revisions)
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue('diff' in result)
+        self.assertEqual(md5(result['diff']).hexdigest(), md5str)
+
+        self._run_svn(['changelist', 'cl1', 'dir1/foo.txt'])
+        revisions = self.client.parse_revision_spec(['cl1'])
+        result = self.client.diff(revisions)
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue('diff' in result)
+        self.assertEqual(md5(result['diff']).hexdigest(), md5str)
+        self._run_svn(['changelist', '--remove', 'dir1/foo.txt'])
+
+        os.chdir('dir2')
+        revisions = self.client.parse_revision_spec()
+        result = self.client.diff(revisions, ['../dir1'])
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue('diff' in result)
+        self.assertEqual(md5(result['diff']).hexdigest(), md5str)
+
+    def test_history_scheduled_with_commit_nominal(self):
+        """Testing SVNClient.history_scheduled_with_commit nominal cases"""
+        self.client.get_repository_info()
+
+        # Ensure valid SVN client version.
+        if not is_valid_version(self.client.subversion_client_version,
+                                self.client.SHOW_COPIES_AS_ADDS_MIN_VERSION):
+            raise SkipTest('Subversion client is too old to test '
+                           'history_scheduled_with_commit().')
+
+        self._svn_add_dir('dir1')
+        self._svn_add_dir('dir2')
+        self._run_svn(['copy', 'foo.txt', 'dir1'])
+
+        # Squash stderr to prevent error message in test output.
+        sys.stderr = StringIO()
+
+        # Ensure SystemExit is raised when attempting to generate diff from
+        # checkout root, via changelist, and via explicit include target.
+
+        revisions = self.client.parse_revision_spec()
+        with self.assertRaises(SystemExit) as cm:
+            self.client.diff(revisions)
+        self.assertEqual(cm.exception.code, 1)
+
+        self._run_svn(['changelist', 'cl1', 'dir1/foo.txt'])
+        revisions = self.client.parse_revision_spec(['cl1'])
+        with self.assertRaises(SystemExit) as cm:
+            self.client.diff(revisions)
+        self.assertEqual(cm.exception.code, 1)
+        self._run_svn(['changelist', '--remove', 'dir1/foo.txt'])
+
+        os.chdir('dir2')
+        revisions = self.client.parse_revision_spec()
+        with self.assertRaises(SystemExit) as cm:
+            self.client.diff(revisions, ['../dir1'])
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_history_scheduled_with_commit_special_case_changelist(self):
+        """Testing SVNClient.history_scheduled_with_commit ignore history in
+        changelist"""
+        self.client.get_repository_info()
+
+        # Ensure valid SVN client version.
+        if not is_valid_version(self.client.subversion_client_version,
+                                self.client.SHOW_COPIES_AS_ADDS_MIN_VERSION):
+            raise SkipTest('Subversion client is too old to test '
+                           'history_scheduled_with_commit().')
+
+        # Add file with history to changelist, then generate diff from checkout
+        # root.  In this case there should be no SystemExit raised and an
+        # (empty) diff should be produced.
+
+        self._run_svn(['copy', 'foo.txt', 'foo_copy.txt'])
+        self._run_svn(['changelist', 'cl1', 'foo_copy.txt'])
+        revisions = self.client.parse_revision_spec()
+        result = self.client.diff(revisions)
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue('diff' in result)
+        self.assertEqual(md5(result['diff']).hexdigest(),
+                         'd41d8cd98f00b204e9800998ecf8427e')
+
+    def test_history_scheduled_with_commit_special_case_exclude(self):
+        """Testing SVNClient.history_scheduled_with_commit with exclude file"""
+        self.client.get_repository_info()
+
+        # Ensure valid SVN client version.
+        if not is_valid_version(self.client.subversion_client_version,
+                                self.client.SHOW_COPIES_AS_ADDS_MIN_VERSION):
+            raise SkipTest('Subversion client is too old to test '
+                           'history_scheduled_with_commit().')
+
+        # Lone file with history is also excluded.  In this case there should
+        # be no SystemExit raised and an (empty) diff should be produced.
+
+        self._run_svn(['copy', 'foo.txt', 'foo_copy.txt'])
+        revisions = self.client.parse_revision_spec()
+        result = self.client.diff(revisions, [], ['foo_copy.txt'])
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue('diff' in result)
+        self.assertEqual(md5(result['diff']).hexdigest(),
+                         'd41d8cd98f00b204e9800998ecf8427e')
 
 
 class P4WrapperTests(RBTestBase):

rbtools/utils/tests.py
@@ -52,6 +52,19 @@ class UtilitiesTest(RBTestBase):
         """Testing 'die' method."""
         self.assertRaises(SystemExit, process.die)
 
+    def test_is_valid_version(self):
+        """Testing 'is_valid_version' method."""
+        self.assertTrue(checks.is_valid_version((1, 0, 0), (1, 0, 0)))
+        self.assertTrue(checks.is_valid_version((1, 1, 0), (1, 0, 0)))
+        self.assertTrue(checks.is_valid_version((1, 0, 1), (1, 0, 0)))
+        self.assertTrue(checks.is_valid_version((1, 1, 0), (1, 1, 0)))
+        self.assertTrue(checks.is_valid_version((1, 1, 1), (1, 1, 0)))
+        self.assertTrue(checks.is_valid_version((1, 1, 1), (1, 1, 1)))
+
+        self.assertFalse(checks.is_valid_version((0, 9, 9), (1, 0, 0)))
+        self.assertFalse(checks.is_valid_version((1, 0, 9), (1, 1, 0)))
+        self.assertFalse(checks.is_valid_version((1, 1, 0), (1, 1, 1)))
+
 
 class AliasTest(RBTestBase):
     """Tests for parameter substitution in rbtools aliases."""

rbtools/clients/git.py
@@ -9,7 +9,7 @@ from rbtools.clients.errors import (MergeError, PushError,
                                     TooManyRevisionsError)
 from rbtools.clients.perforce import PerforceClient
 from rbtools.clients.svn import SVNClient, SVNRepositoryInfo
-from rbtools.utils.checks import check_install
+from rbtools.utils.checks import check_install, is_valid_version
 from rbtools.utils.console import edit_text
 from rbtools.utils.diffs import (normalize_patterns,
                                  remove_filenames_matching_patterns)
@@ -251,10 +251,10 @@ class GitClient(SCMClient):
                     ignore_errors=True)
 
                 if (version_parts and svn_remote and
-                    not self.is_valid_version((int(version_parts.group(1)),
-                                               int(version_parts.group(2)),
-                                               int(version_parts.group(3))),
-                                              (1, 5, 4))):
+                    not is_valid_version((int(version_parts.group(1)),
+                                          int(version_parts.group(2)),
+                                          int(version_parts.group(3))),
+                                         (1, 5, 4))):
                     die("Your installation of git-svn must be upgraded to "
                         "version 1.5.4 or later")
 
@@ -338,18 +338,6 @@ class GitClient(SCMClient):
             [self.git, "config", "--get", "remote.%s.url" % upstream_remote],
             ignore_errors=True).rstrip("\n")
         return (upstream_branch, origin_url)
-
-    def is_valid_version(self, actual, expected):
-        """
-        Takes two tuples, both in the form:
-            (major_version, minor_version, micro_version)
-        Returns true if the actual version is greater than or equal to
-        the expected version, and false otherwise.
-        """
-        return ((actual[0] > expected[0]) or
-                (actual[0] == expected[0] and actual[1] > expected[1]) or
-                (actual[0] == expected[0] and actual[1] == expected[1] and
-                 actual[2] >= expected[2]))
 
     def scan_for_server(self, repository_info):
         # Scan first for dot files, since it's faster and will cover the

commit 9d1e64a1045217ba67e824f80084cb39494e0719

Correct minor issues with history scheduled with commit processing for SVN.
For Subversion there is special processing to determine when a changeset has
history scheduled with commit, which in turn requires the
`--svn-show-copies-as-adds` option to be specified. There are two issues with
this processing:

1. A logic fault was introduced in (e20a1ba) related to Subversion changelists.
   When `svn diff` is executed with the `--changelist` option then only files
   that are members of that changelist are included in the diff.  But, the
   inverse is not true. That is, if a working copy contains modified files
   that are not members of any changelist as well as other modified files that
   are members of a changelist, then `svn diff` (with no extra options) will
   produce a diff for **all** modified files, regardless of changelist
   membership. In (e20a1ba) code was added for the case of not working with a
   changelist, to return early from parsing `svn status` output when the
   changelist "section" of the status output was encountered. This early exist
   should not occur as even files in changelists must be considered for having
   history scheduled with commit.

2. `history_scheduled_with_commit()` parses the output of `svn status` to find
   if any file status has '+' in 4th column. `svn status` is only pertinent to
   working copies, however there is no code in place to prevent executing
   `history_scheduled_with_commit()` when generating a diff between specific
   number revisions. This is problematic when executing rbt within a working
   copy with history scheduled with commit, but attempting to diff/post not
   against the working copy but instead between two revisions (e.g.
   `rbt post 10:12`). In this case the diff generation will terminate
   indicating that `--svn-show-copies-as-adds` must be specified, which is
   both confusing and wrong because it depends on the state of the current

rbtools/clients/svn.py
@@ -330,8 +330,15 @@ class SVNClient(SCMClient):
 
         diff_cmd.extend(include_files)
 
-        if is_valid_version(self.subversion_client_version,
-                            self.SHOW_COPIES_AS_ADDS_MIN_VERSION):
+        # Check for and validate --svn-show-copies-as-adds option, or evaluate
+        # working copy to determine if scheduled commit will contain
+        # addition-with-history commit. When this case occurs then
+        # --svn-show-copies-as-adds must be specified. Note: this only
+        # pertains to local modifications in a working copy and not diffs
+        # between specific numeric revisions.
+        if (((tip == self.REVISION_WORKING_COPY) or changelist) and
+            is_valid_version(self.subversion_client_version,
+                             self.SHOW_COPIES_AS_ADDS_MIN_VERSION)):
             svn_show_copies_as_adds = getattr(
                 self.options, 'svn_show_copies_as_adds', None)
 
@@ -379,13 +386,6 @@ class SVNClient(SCMClient):
         for p in self._run_svn(status_cmd, split_lines=True,
                                results_unicode=False):
             try:
-                if not changelist and p.startswith(b'--- Changelist'):
-                    # svn status returns changelist information last.  If we
-                    # hit a line starting with '--- Changelist' then we have
-                    # reached the changelist section and can exit the loop as
-                    # we are not interested in changelists.
-                    break
-
                 if p[3] == b'+':
                     if exclude_patterns:
                         # We found a file with history, but first we must make

rbtools/clients/tests.py
@@ -1521,7 +1521,7 @@ class SVNClientTests(SCMClientTests):
         self.check_show_copies_as_adds('n', 'd41d8cd98f00b204e9800998ecf8427e')
 
     def check_show_copies_as_adds(self, state, md5str):
-        """Helper function to evaluate --show-copies as adds"""
+        """Helper function to evaluate --show-copies-as-adds"""
         self.client.get_repository_info()
 
         # Ensure valid SVN client version.
@@ -1536,8 +1536,11 @@ class SVNClientTests(SCMClientTests):
         self._svn_add_dir('dir2')
         self._run_svn(['copy', 'foo.txt', 'dir1'])
 
-        # Generate identical diff from checkout root, via changelist, and via
-        # explicit include target.
+        # Generate identical diff via several methods:
+        #  1) from checkout root
+        #  2) via changelist
+        #  3) from checkout root when all relevant files belong to a changelist
+        #  4) via explicit include target
 
         revisions = self.client.parse_revision_spec()
         result = self.client.diff(revisions)
@@ -1551,6 +1554,13 @@ class SVNClientTests(SCMClientTests):
         self.assertTrue(isinstance(result, dict))
         self.assertTrue('diff' in result)
         self.assertEqual(md5(result['diff']).hexdigest(), md5str)
+
+        revisions = self.client.parse_revision_spec()
+        result = self.client.diff(revisions)
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue('diff' in result)
+        self.assertEqual(md5(result['diff']).hexdigest(), md5str)
+
         self._run_svn(['changelist', '--remove', 'dir1/foo.txt'])
 
         os.chdir('dir2')
@@ -1577,8 +1587,12 @@ class SVNClientTests(SCMClientTests):
         # Squash stderr to prevent error message in test output.
         sys.stderr = StringIO()
 
-        # Ensure SystemExit is raised when attempting to generate diff from
-        # checkout root, via changelist, and via explicit include target.
+        # Ensure SystemExit is raised when attempting to generate diff via
+        # several methods:
+        #  1) from checkout root
+        #  2) via changelist
+        #  3) from checkout root when all relevant files belong to a changelist
+        #  4) via explicit include target
 
         revisions = self.client.parse_revision_spec()
         self.assertRaises(SystemExit, self.client.diff, revisions)
@@ -1587,15 +1601,18 @@ class SVNClientTests(SCMClientTests):
         revisions = self.client.parse_revision_spec(['cl1'])
         self.assertRaises(SystemExit, self.client.diff, revisions)
 
+        revisions = self.client.parse_revision_spec()
+        self.assertRaises(SystemExit, self.client.diff, revisions)
+
         self._run_svn(['changelist', '--remove', 'dir1/foo.txt'])
 
         os.chdir('dir2')
         revisions = self.client.parse_revision_spec()
         self.assertRaises(SystemExit, self.client.diff, revisions, ['../dir1'])
 
-    def test_history_scheduled_with_commit_special_case_changelist(self):
-        """Testing SVNClient.history_scheduled_with_commit ignore history in
-        changelist"""
+    def test_history_scheduled_with_commit_special_case_non_local_mods(self):
+        """Testing SVNClient.history_scheduled_with_commit is bypassed when
+        diff is not for local modifications in a working copy"""
         self.client.get_repository_info()
 
         # Ensure valid SVN client version.
@@ -1604,18 +1621,26 @@ class SVNClientTests(SCMClientTests):
             raise SkipTest('Subversion client is too old to test '
                            'history_scheduled_with_commit().')
 
-        # Add file with history to changelist, then generate diff from checkout
-        # root.  In this case there should be no SystemExit raised and an
-        # (empty) diff should be produced.
+        # While within a working copy which contains a scheduled commit with
+        # addition-with-history, ensure history_scheduled_with_commit() is not
+        # executed when generating a diff between two revisions either
+        # 1) locally or 2) via --reposistory-url option.
 
         self._run_svn(['copy', 'foo.txt', 'foo_copy.txt'])
-        self._run_svn(['changelist', 'cl1', 'foo_copy.txt'])
-        revisions = self.client.parse_revision_spec()
+        revisions = self.client.parse_revision_spec(['1:2'])
         result = self.client.diff(revisions)
         self.assertTrue(isinstance(result, dict))
         self.assertTrue('diff' in result)
         self.assertEqual(md5(result['diff']).hexdigest(),
-                         'd41d8cd98f00b204e9800998ecf8427e')
+                         'ed154720a7459c2649cab4d2fa34fa93')
+
+        self.options.repository_url = self.svn_repo_url
+        revisions = self.client.parse_revision_spec(['2'])
+        result = self.client.diff(revisions)
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue('diff' in result)
+        self.assertEqual(md5(result['diff']).hexdigest(),
+                         'ed154720a7459c2649cab4d2fa34fa93')
 
     def test_history_scheduled_with_commit_special_case_exclude(self):
         """Testing SVNClient.history_scheduled_with_commit with exclude file"""
@@ -1628,10 +1653,19 @@ class SVNClientTests(SCMClientTests):
                            'history_scheduled_with_commit().')
 
         # Lone file with history is also excluded.  In this case there should
-        # be no SystemExit raised and an (empty) diff should be produced.
+        # be no SystemExit raised and an (empty) diff should be produced. Test
+        # from checkout root and via changelist.
 
         self._run_svn(['copy', 'foo.txt', 'foo_copy.txt'])
         revisions = self.client.parse_revision_spec([])
+        result = self.client.diff(revisions, [], ['foo_copy.txt'])
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue('diff' in result)
+        self.assertEqual(md5(result['diff']).hexdigest(),
+                         'd41d8cd98f00b204e9800998ecf8427e')
+
+        self._run_svn(['changelist', 'cl1', 'foo_copy.txt'])
+        revisions = self.client.parse_revision_spec(['cl1'])
         result = self.client.diff(revisions, [], ['foo_copy.txt'])
         self.assertTrue(isinstance(result, dict))
         self.assertTrue('diff' in result)