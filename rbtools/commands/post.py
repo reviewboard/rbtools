4e01cf8007e430b568dd3eac9bdf3530c263fc4a
David Trowbridge <trowbrds@gmail.com>
2020-08-18 15:13:30 -0600

    Fix a couple problems in rbt post error handling.
    When rbt post encounters an error when creating the review request,
    there were a couple problems that could manifest:
    
    - The code that attempted to handle the case of updating an existing
      review request on services that use changenums (perforce, plastic) was
      using an old variable name, causing a `NameError`.
    - The error message formatting for other types of `APIError`s was
      passing the exception instance into `force_unicode`, which only
      accepts unicode or bytes. This has been changed to cast first to call
      the `__str__` method.
    
    Testing Done:
    - Ran unit tests.
    - Smoke tested rbt post error handling.
    
    Fixes bug 4858.
    
    Reviewed at https://reviews.reviewboard.org/r/11142/

--- rbtools/commands/post.py
+++ rbtools/commands/post.py
@@ -684,7 +684,7 @@ class Post(Command):
 
                     try:
                         review_request = api_root.get_review_request(
-                            review_request_id=rid,
+                            review_request_id=review_request_id,
                             only_fields='absolute_url,bugs_closed,id,status',
                             only_links='diffs,draft')
                     except APIError as e:
@@ -730,7 +730,7 @@ class Post(Command):
                     'usually means no files were'
                     'modified in this change.')
             else:
-                error_msg.append(force_unicode(e))
+                error_msg.append(force_unicode(str(e)))
 
             error_msg.append(
                 'Your review request still exists, but the diff is '
