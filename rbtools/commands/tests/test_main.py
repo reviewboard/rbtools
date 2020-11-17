ac663b359b9632ca4a76700f8b6dd1617e4da349
Christian Hammond <christian@beanbaginc.com>
2020-11-17 02:31:45 -0800

    Fix issues testing rbt with the right environment.
    The unit tests for the `rbt` command weren't always running the correct
    version of RBTools. The main test suite was running whatever `rbt` was
    in the path with whatever version of Python it was installed with, which
    didn't guarantee correct results, while the `--version` tests ran with
    the current Python but also ran the first build found in the path.
    
    These could produce wrong results, and in fact have in my own test
    environment. To fix this, we now run the bundled copy specifically, and
    we now always do so with the current version of Python. This ensures a
    reliable test run, and also allows us to consolidate the two test suites
    for this command.
    
    Testing Done:
    Unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/11275/

--- rbtools/commands/tests/test_main.py
+++ rbtools/commands/tests/test_main.py
@@ -6,63 +6,70 @@ import os.path
 import sys
 
 from rbtools import get_version_string
+from rbtools.commands import main as rbt_main
 from rbtools.utils.process import execute
 from rbtools.utils.testbase import RBTestBase
 
 
-class HelpCommandTests(RBTestBase):
+_rbt_path = rbt_main.__file__
+
+
+class MainCommandTests(RBTestBase):
     """Tests for RBT help command and rbt command help options."""
 
     def test_help_command(self):
         """Testing RBT commands when running 'rbt help <command>'"""
-        self._check_help_output(['rbt', 'help', 'alias'], 'alias')
+        self._check_help_output(['help', 'alias'], 'alias')
 
     def test_help_options_before(self):
         """Testing RBT commands when running 'rbt --help <command>' or 'rbt
         -h <command>'
         """
-        self._check_help_output(['rbt', '--help', 'alias'], 'alias')
-        self._check_help_output(['rbt', '-h', 'alias'], 'alias')
+        self._check_help_output(['--help', 'alias'], 'alias')
+        self._check_help_output(['-h', 'alias'], 'alias')
 
     def test_help_options_after(self):
         """Testing RBT commands when running 'rbt <command> --help' or 'rbt
         <command> -h'
-         """
-        self._check_help_output(['rbt', 'alias', '--help'], 'alias')
-        self._check_help_output(['rbt', 'alias', '-h'], 'alias')
+        """
+        self._check_help_output(['alias', '--help'], 'alias')
+        self._check_help_output(['alias', '-h'], 'alias')
 
     def test_help_invalid_command(self):
         """Testing RBT commands when running '--help' or '-h' with an
         invalid command
-         """
-        self._check_help_output(['rbt', 'invalid', '--help'],
+        """
+        self._check_help_output(['invalid', '--help'],
                                 'invalid', invalid=True)
-        self._check_help_output(['rbt', 'invalid', '-h'], 'invalid',
+        self._check_help_output(['invalid', '-h'], 'invalid',
                                 invalid=True)
-        self._check_help_output(['rbt', 'help', 'invalid'], 'invalid',
+        self._check_help_output(['help', 'invalid'], 'invalid',
                                 invalid=True)
 
     def test_help_multiple_args(self):
         """Testing RBT commands when running the help command or help
         options with multiple arguments present
         """
-        self._check_help_output(['rbt', 'alias', 'extra_arg', '--help'],
-                                'alias')
-        self._check_help_output(['rbt', 'alias', 'extra_arg', '-h'], 'alias')
-        self._check_help_output(['rbt', 'alias', '--help', 'extra_arg'],
-                                'alias')
-        self._check_help_output(['rbt', 'alias', '-h', 'extra_arg'], 'alias')
-        self._check_help_output(['rbt', '--help', 'alias', 'extra_arg'],
-                                'alias')
-        self._check_help_output(['rbt', '-h', 'alias', 'extra_arg'], 'alias')
-        self._check_help_output(['rbt', 'help', 'alias', 'extra_arg'], 'alias')
+        self._check_help_output(['alias', 'extra_arg', '--help'], 'alias')
+        self._check_help_output(['alias', 'extra_arg', '-h'], 'alias')
+        self._check_help_output(['alias', '--help', 'extra_arg'], 'alias')
+        self._check_help_output(['alias', '-h', 'extra_arg'], 'alias')
+        self._check_help_output(['--help', 'alias', 'extra_arg'], 'alias')
+        self._check_help_output(['-h', 'alias', 'extra_arg'], 'alias')
+        self._check_help_output(['help', 'alias', 'extra_arg'], 'alias')
 
-    def _check_help_output(self, command, subcommand, invalid=False):
+    def test_version_command(self):
+        """Testing RBT commands when running 'rbt --version' and 'rbt -v"
+        """
+        self._check_version_output('--version')
+        self._check_version_output('-v')
+
+    def _check_help_output(self, args, subcommand, invalid=False):
         """Check if a specific rbt command's output exists in test output.
 
         Args:
-            command (list of unicode):
-                The rbt command used for testing.
+            args (list of unicode):
+                The ``rbt`` command arguments.
 
             subcommand (unicode)
                 The unicode string of the rbt command type.
@@ -72,51 +79,42 @@ class HelpCommandTests(RBTestBase):
                 running an invalid command. Otherwise, check if output
                 matches what is expected after running a valid rbt command.
         """
-        try:
-            output = execute(command)
-        except Exception as e:
-            self.fail(e)
+        output = self._run_rbt(*args)
 
         if invalid:
             self.assertIn('No help found for %s' % subcommand, output)
         else:
             self.assertIn('usage: rbt %s [options]' % subcommand, output)
 
-
-class VersionCommandTests(RBTestBase):
-    """Tests for RBT --version command and rbt command version options."""
-
-    def test_version_command(self):
-        """Testing RBT commands when running 'rbt --version' and 'rbt -v"
-        """
-        # Unlike most of the other tests that can invoke `rbt` directly and
-        # expect reasonable results, we need to make sure that this one is
-        # executed using the same version of Python as we're currently using.
-        rbt_path = None
-
-        for dir in os.environ['PATH'].split(os.pathsep):
-            path = os.path.join(dir, 'rbt')
-
-            if os.path.exists(path):
-                rbt_path = path
-
-        self._check_version_output([sys.executable, rbt_path, '--version'])
-        self._check_version_output([sys.executable, rbt_path, '-v'])
-
-    def _check_version_output(self, command):
-        """Check if a correct rbt version and python version exist in test output.
+    def _check_version_output(self, version_arg):
+        """Check if RBTools reports the correct version information.
 
         Args:
-            command (list of unicode):
-                The rbt command used for testing.
+            version_arg (unicode):
+                The version flag to pass to ``rbt``.
         """
-        try:
-            output = execute(command)
-        except Exception as e:
-            self.fail(e)
+        output = self._run_rbt(version_arg)
 
-        self.assertEqual('RBTools %s (Python %d.%d.%d)\n' % (
-            get_version_string(),
-            sys.version_info[:3][0],
-            sys.version_info[:3][1],
-            sys.version_info[:3][2]), output)
+        self.assertEqual(
+            output,
+            'RBTools %s (Python %d.%d.%d)\n'
+            % (get_version_string(),
+               sys.version_info[:3][0],
+               sys.version_info[:3][1],
+               sys.version_info[:3][2]))
+
+    def _run_rbt(self, *args):
+        """Run rbt with the current Python and provided arguments.
+
+        This will ensure the correct version of ``rbt`` is being run and with
+        the current version of Python.
+
+        Args:
+            *args (tuple):
+                The command line arguments to pass to ``rbt``.
+
+        Returns:
+            unicode:
+            The resulting output from the command.
+        """
+        return execute([sys.executable, _rbt_path] + list(args))
