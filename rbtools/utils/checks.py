aa07d38a1a838d8a8c2d813f3427f5880a000e7e
Ruonan Jia <ruonanjia54@gmail.com>
2021-10-26 13:07:37 -0600

    Properly handle missing executables when pyenv intercepts.
    When pyenv is in use, and the needed executable is only available in a
    different Python version, pyenv will intercept the call and print an
    error (as well as returning 127). This means that attempts to execute
    something like, say, `bzr`, will fail with the following error:
    
    ```
    pyenv: bzr: command not found
    
    The `bzr' command exists in these Python versions:
      2.7.14
    ```
    
    This was causing our existing checks to fail with a false positive.
    
    Testing Done:
    Tested `rbt post` manually in my local integrations repo with the
    changes.
    
    Reviewed at https://reviews.reviewboard.org/r/11196/

--- rbtools/utils/checks.py
+++ rbtools/utils/checks.py
@@ -23,11 +23,16 @@ def check_install(command):
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
 
-        # Wait for the process to end and pipes to close. This will avoid
-        # ResourceWarnings.
-        p.communicate()
+        stdout, stderr = p.communicate()
 
-        return True
+        # When pyenv is in use, and the needed executable is only available in
+        # a different Python version, pyenv intercepts the call and prints an
+        # error/returns 127. This prevents us from getting any of the expected
+        # exceptions.
+        if b'command not found' in stderr:
+            return False
+        else:
+            return (p.returncode != 127)
     except (OSError, ValueError):
         # We catch ValueError exceptions here to work around bug in the
         # version of Python that ships with OS X 10.11. I don't know if the
