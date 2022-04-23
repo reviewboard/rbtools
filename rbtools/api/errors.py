5d3b3880d7be50235a07fccf82e5ac9a661bac3a
Christian Hammond <christian@beanbaginc.com>
2022-04-22 17:15:16 -0700

    Improve API error responses and fix error-related crashes.
    When attempting to debug an authentication issue, I encountered a case
    where formatting an API response would crash. This was due to the
    particular error case raising an `AuthorizationError` with
    `http_status=None`, and string building expecting a valid integer.
    
    In the process of fixing this, I decided to change how we handle API
    error reporting, in order to reduce some corner cases that could occur
    with less-than-helpful errors.
    
    The following improvements have been made to the error message string
    building:
    
    * If the API error code is available, the HTTP status code will not be
      shown (as codes like "Internal Server Error" confuse people).
    
    * If neither an API error code nor HTTP status code are available,
      the code details will not be shown (and will not cause a crash).
    
    * When showing either an API or HTTP status code, the name of the error
      or HTTP status is shown alongside the code, if known. This uses a
      built-in map of codes to names.
    
    * An explicit message is now always shown. In order of priority, this
      can be a message passed to the constructor, the message from the
      response payload, or a default message set on the `APIError` class or
      subclass.
    
    There was some back-and-forth on my end with whether we should continue
    to even show the API error code information, but it can aid in
    debugging, so it's being left in. When present, the HTTP status code can
    be inferred, hence its removal in this situation.
    
    Unit tests now exist for all error classes and their string building.
    
    Testing Done:
    Unit tests passed.
    
    Triggered several API errors (including the one one that originally
    crashed) and saw improved results.
    
    Reviewed at https://reviews.reviewboard.org/r/12244/

--- rbtools/api/errors.py
+++ rbtools/api/errors.py
@@ -5,37 +5,283 @@ import six
 from rbtools.utils.encoding import force_unicode
 
 
+HTTP_STATUS_CODES = {
+    100: 'Continue',
+    101: 'Switching Protocols',
+    102: 'Processing',
+    103: 'Early Hints',
+    200: 'OK',
+    201: 'Created',
+    202: 'Accepted',
+    203: 'Non-Authoritative Information',
+    204: 'No Content',
+    205: 'Reset Content',
+    206: 'Partial Content',
+    207: 'Multi-Status',
+    208: 'Already Reported',
+    226: 'IM Used',
+    300: 'Multiple Choices',
+    301: 'Moved Permanently',
+    302: 'Found',
+    303: 'See Other',
+    304: 'Not Modified',
+    305: 'Use Proxy',
+    306: 'Switch Proxy',
+    307: 'Temporary Redirect',
+    308: 'Permanent Redirect',
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    402: 'Payment Required',
+    403: 'Forbidden',
+    404: 'Not Found',
+    405: 'Method Not Allowed',
+    406: 'Not Acceptable',
+    407: 'Proxy Authentication Required',
+    408: 'Request Timeout',
+    409: 'Conflict',
+    410: 'Gone',
+    411: 'Length Required',
+    412: 'Precondition Failed',
+    413: 'Request Entity Too Large',
+    414: 'Request-URI Too Long',
+    415: 'Unsupported Media Type',
+    416: 'Requested Range Not Satisfiable',
+    417: 'Expectation Failed',
+    418: "I'm a teapot",
+    421: 'Misdirected Request',
+    422: 'Unprocessable Entity',
+    423: 'Locked',
+    424: 'Failed Dependency',
+    425: 'Unordered Collection',
+    426: 'Upgrade Required',
+    428: 'Precondition Required',
+    429: 'Too Many Requests',
+    431: 'Request Header Fields Too Large',
+    444: 'No Response',
+    449: 'Retry With',
+    450: 'Blocked by Windows Parental Controls',
+    451: 'Unavailable For Legal Reasons',
+    499: 'Client Closed Request',
+    500: 'Internal Server Error',
+    501: 'Not Implemented',
+    502: 'Bad Gateway',
+    503: 'Service Unavailable',
+    504: 'Gateway Timeout',
+    505: 'HTTP Version Not Supported',
+    506: 'Variant Also Negotiates',
+    507: 'Insufficient Storage',
+    508: 'Loop Detected',
+    509: 'Bandwidth Limit Exceeded',
+    510: 'Not Extended',
+    511: 'Network Authentication Required',
+    598: 'Network Read Timeout Error',
+    599: 'Network Connect Timeout Error',
+}
+
+
+API_ERROR_CODES = {
+    0: 'No Error',
+    1: 'Service Not Configured',
+    100: 'Does Not Exist',
+    101: 'Permission Denied',
+    102: 'Invalid Attribute',
+    103: 'Not Logged In',
+    104: 'Login Failed',
+    105: 'Invalid Form Data',
+    106: 'Missing Attribute',
+    107: 'Enable Extension Failed',
+    108: 'Disable Extension Failed',
+    109: 'Extension Already Installed',
+    110: 'Extension Install Failed',
+    111: 'Duplicate Item',
+    112: 'OAuth Scope Missing',
+    113: 'OAuth Token Access Denied',
+    114: 'API Rate Limit Exceeded',
+    200: 'Unspecified Diff Revision',
+    201: 'Invalid Diff Revision',
+    202: 'Invalid Action Specified',
+    203: 'Invalid Commit ID',
+    204: 'Commit ID In Use',
+    205: 'Missing Repository',
+    206: 'Invalid Repository',
+    207: 'Repository File Not Found',
+    208: 'Invalid User',
+    209: 'Repository Operation Not Implemented',
+    210: 'Repository Information Error',
+    211: 'Nothing To Publish',
+    212: 'Empty Changeset',
+    213: 'Server Configuration Error',
+    214: 'Bad Host Key',
+    215: 'Unverified Host Key',
+    216: 'Unverified Host Certificate',
+    217: 'Missing User Key',
+    218: 'Repository Authentication Failed',
+    219: 'Empty Diff',
+    220: 'Diff Too Big',
+    221: 'File Retrieval Failed',
+    222: 'Hosting Service Authentication Failed',
+    223: 'Group Already Exists',
+    224: 'Diff Parsing Failed',
+    225: 'Publish Failed',
+    226: 'User Query Failed',
+    227: 'Commit ID Already Exists',
+    228: 'API Token Generation Failed',
+    229: 'Repository Already Exists',
+    230: 'Close Review Request Failed',
+    231: 'Reopen Review Request Failed',
+    232: 'Revoke Ship-It Failed',
+    233: 'Server Is Read Only',
+}
+
+
 class APIError(Exception):
-    def __init__(self, http_status, error_code, rsp=None, *args, **kwargs):
+    """An error communicating with the API.
+
+    Attributes:
+        error_code (int):
+            The API error code. This may be ``None``.
+
+        http_status (int):
+            The HTTP status code.
+
+        message (unicode):
+            The error message from the API response. This may be ``None``.
+
+            Version Added:
+                3.1
+
+        rsp (dict):
+            The API response payload. This may be ``None``.
+    """
+
+    #: The default error message used if a specific error is not available.
+    #:
+    #: Version Added:
+    #:     3.1
+    #:
+    #: Type:
+    #:     unicode
+    default_message = 'An error occurred when communicating with Review Board.'
+
+    def __init__(self, http_status=None, error_code=None, rsp=None,
+                 message=None, *args, **kwargs):
+        """Initialize the error.
+
+        Args:
+            http_status (int, optional):
+                The HTTP status code associated with this error.
+
+                Version Changed:
+                    3.1:
+                    This is now optional.
+
+            error_code (int, optional):
+                The API error code associated with this error.
+
+                Version Changed:
+                    3.1:
+                    This is now optional.
+
+            rsp (dict, optional):
+                The API response payload. This may be ``None`` for non-API
+                Error payloads.
+
+            message (unicode, optional):
+                A specific error message to use. This will take precedence
+                over any errors in ``rsp``.
+
+                Version Added:
+                    3.1
+
+            *args (tuple):
+                Extra positional arguments to pass to the base constructor.
+
+            **kwargs (dict):
+                Extra keyword arguments to pass to the base constructor.
+        """
         Exception.__init__(self, *args, **kwargs)
         self.http_status = http_status
         self.error_code = error_code
         self.rsp = rsp
 
+        if rsp and not message:
+            try:
+                message = rsp['err']['msg']
+            except KeyError:
+                message = None
+
+        self.message = message or self.default_message
+
     def __str__(self):
-        code_str = 'HTTP %d' % self.http_status
+        """Return a string representation of the error.
 
-        if self.error_code:
-            code_str += ', API Error %d' % self.error_code
+        The explicit :py:attr:`message` passed to the constructor will be
+        used if provided. If not provided, this will fall back to the
+        message in the :py:attr:`rsp` payload, or to
+        :py:attr:`default_message`.
 
-        if self.rsp and 'err' in self.rsp:
-            return '%s (%s)' % (self.rsp['err']['msg'], code_str)
-        else:
-            return code_str
+        If an API error code is available, it will be included in the message.
+        If one is not provided, but an HTTP status is available, then it will
+        be included instead.
+
+        Returns:
+            unicode:
+            The error message.
+        """
+        http_status = self.http_status
+        error_code = self.error_code
+
+        details = None
+
+        if self.error_code is not None:
+            error_name = API_ERROR_CODES.get(error_code)
+            details = 'API Error %s' % error_code
+
+            if error_name:
+                details = '%s: %s' % (details, error_name)
+        elif http_status is not None:
+            http_status_name = HTTP_STATUS_CODES.get(http_status)
+            details = 'HTTP %s' % http_status
+
+            if http_status_name:
+                details = '%s: %s' % (details, http_status_name)
+
+        message = self.message
+
+        if details:
+            message = '%s (%s)' % (message, details)
+
+        return message
 
 
 class AuthorizationError(APIError):
-    pass
+    """Authorization error when communicating with the API."""
+
+    default_message = 'Error authenticating to Review Board.'
 
 
 class BadRequestError(APIError):
+    """Bad request data made to an API."""
+
+    default_message = 'Missing or invalid data was sent to Review Board.'
+
     def __str__(self):
+        """Return a string representation of the error.
+
+        If the payload contains a list of fields, the error associated with
+        each field will be included.
+
+        Returns:
+            unicode:
+            The error message.
+        """
         lines = [super(BadRequestError, self).__str__()]
 
         if self.rsp and 'fields' in self.rsp:
             lines.append('')
 
-            for field, error in six.iteritems(self.rsp['fields']):
+            for field, error in sorted(six.iteritems(self.rsp['fields']),
+                                       key=lambda pair: pair[0]):
                 lines.append('    %s: %s' % (field, '; '.join(error)))
 
         return '\n'.join(lines)
---
+++ rbtools/api/tests/test_errors.py
@@ -0,0 +1,176 @@
+"""Unit tests for rbtools.api.errors."""
+
+from __future__ import unicode_literals
+
+import six
+
+from rbtools.api.errors import APIError, AuthorizationError, BadRequestError
+from rbtools.testing import TestCase
+
+
+class APIErrorTests(TestCase):
+    """Unit tests for rbtools.api.errors.APIError."""
+
+    def test_str_with_http_status(self):
+        """Testing APIError.__str__ with http_status"""
+        self.assertEqual(
+            six.text_type(APIError(http_status=500)),
+            'An error occurred when communicating with Review Board. '
+            '(HTTP 500: Internal Server Error)')
+
+    def test_str_with_http_status_unknown(self):
+        """Testing APIError.__str__ with unknown http_status"""
+        self.assertEqual(
+            six.text_type(APIError(http_status=900)),
+            'An error occurred when communicating with Review Board. '
+            '(HTTP 900)')
+
+    def test_str_with_error_code(self):
+        """Testing APIError.__str__ with error_code"""
+        self.assertEqual(
+            six.text_type(APIError(error_code=105)),
+            'An error occurred when communicating with Review Board. '
+            '(API Error 105: Invalid Form Data)')
+
+    def test_str_with_error_code_unknown(self):
+        """Testing APIError.__str__ with unknown error_code"""
+        self.assertEqual(
+            six.text_type(APIError(error_code=12345)),
+            'An error occurred when communicating with Review Board. '
+            '(API Error 12345)')
+
+    def test_str_with_http_status_and_error_code(self):
+        """Testing APIError.__str__ with http_status and error_code"""
+        self.assertEqual(
+            six.text_type(APIError(http_status=400,
+                                   error_code=106)),
+            'An error occurred when communicating with Review Board. '
+            '(API Error 106: Missing Attribute)')
+
+    def test_str_with_rsp(self):
+        """Testing APIError.__str__ with rsp error message"""
+        self.assertEqual(
+            six.text_type(APIError(rsp={
+                'err': {
+                    'msg': 'Bad things happened.',
+                },
+            })),
+            'Bad things happened.')
+
+    def test_str_with_rsp_and_error_code(self):
+        """Testing APIError.__str__ with rsp error message and error_code"""
+        self.assertEqual(
+            six.text_type(APIError(
+                http_status=400,
+                error_code=106,
+                rsp={
+                    'err': {
+                        'msg': 'Bad things happened.',
+                    },
+                })),
+            'Bad things happened. (API Error 106: Missing Attribute)')
+
+    def test_str_with_rsp_and_http_status(self):
+        """Testing APIError.__str__ with rsp error message and http_status"""
+        self.assertEqual(
+            six.text_type(APIError(
+                http_status=400,
+                rsp={
+                    'err': {
+                        'msg': 'Bad things happened.',
+                    },
+                })),
+            'Bad things happened. (HTTP 400: Bad Request)')
+
+    def test_str_with_no_details(self):
+        """Testing APIError.__str__ without any details"""
+        self.assertEqual(
+            six.text_type(APIError()),
+            'An error occurred when communicating with Review Board.')
+
+
+class AuthorizationErrorTests(TestCase):
+    """Unit tests for rbtools.api.errors.AuthorizationError."""
+
+    def test_str_with_message(self):
+        """Testing AuthorizationError.__str__ with explicit error message"""
+        self.assertEqual(
+            six.text_type(AuthorizationError(message='Oh no.')),
+            'Oh no.')
+
+    def test_str_with_details(self):
+        """Testing AuthorizationError.__str__ without explicit error message,
+        with HTTP details
+        """
+        self.assertEqual(
+            six.text_type(AuthorizationError(http_status=401,
+                                             error_code=104)),
+            'Error authenticating to Review Board. (API Error 104: '
+            'Login Failed)')
+
+    def test_str_without_message_or_details(self):
+        """Testing AuthorizationError.__str__ without explicit error message
+        or HTTP details
+        """
+        self.assertEqual(
+            six.text_type(AuthorizationError()),
+            'Error authenticating to Review Board.')
+
+
+class BadRequestErrorTests(TestCase):
+    """Unit tests for rbtools.api.errors.BadRequestError."""
+
+    def test_str(self):
+        """Testing BadRequestError.__str__"""
+        self.assertEqual(
+            six.text_type(BadRequestError()),
+            'Missing or invalid data was sent to Review Board.')
+
+    def test_str_with_error_code(self):
+        """Testing BadRequestError.__str__"""
+        self.assertEqual(
+            six.text_type(BadRequestError(error_code=200)),
+            'Missing or invalid data was sent to Review Board. '
+            '(API Error 200: Unspecified Diff Revision)')
+
+    def test_str_with_rsp_error_message(self):
+        """Testing BadRequestError.__str__ with rsp error message"""
+        self.assertEqual(
+            six.text_type(BadRequestError(
+                error_code=200,
+                rsp={
+                    'err': {
+                        'msg': 'Diff revision not specified.',
+                    },
+                })),
+            'Diff revision not specified. (API Error 200: Unspecified Diff '
+            'Revision)')
+
+    def test_str_with_message(self):
+        """Testing BadRequestError.__str__ with message"""
+        self.assertEqual(
+            six.text_type(BadRequestError(
+                error_code=200,
+                message='Diff revision not specified.')),
+            'Diff revision not specified. (API Error 200: Unspecified Diff '
+            'Revision)')
+
+    def test_str_with_message_with_fields(self):
+        """Testing BadRequestError.__str__ with fields"""
+        self.assertEqual(
+            six.text_type(BadRequestError(
+                error_code=105,
+                rsp={
+                    'err': {
+                        'msg': 'One or more fields had errors',
+                    },
+                    'fields': {
+                        'field1': ['This field was invalid'],
+                        'field2': ['This one, too', 'So invalid'],
+                    },
+                })),
+            'One or more fields had errors (API Error 105: Invalid Form '
+            'Data)\n'
+            '\n'
+            '    field1: This field was invalid\n'
+            '    field2: This one, too; So invalid')
