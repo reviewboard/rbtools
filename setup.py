dcc36867581f31286e474e80f4f11d43516b2ea8
Christian Hammond <christian@beanbaginc.com>
2022-08-15 19:14:41 -0700

    Ensure usage of modern packages when using 'setup.py develop'.
    The Python packaging world has long since recommended against eggs and
    `setup.py develop`. However, until we move to a `pyproject.toml` with
    a custom build system, this is our best option.
    
    Review Board, Djblets, and other packages have implemented a custom
    `setup.py develop` for ages, which wraps `pip install -e .` (the
    generally preferred method) and then installs our development
    dependencies.
    
    This change adds the same logic to the RBTools packaging. This will
    ensure that we don't ever introduce legacy eggs through RBTools for
    development, and always prefer model wheels.
    
    For now, `setup.py develop` is the preferred way of preparing RBTools
    for development. In time, we will transition away from `setup.py` and
    will prefer `pip install -e .`.
    
    Testing Done:
    Successfully installed RBTools on Python 3.7-3.11, with all dependencies
    as wheels, using `setup.py develop`.
    
    Reviewed at https://reviews.reviewboard.org/r/12524/

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+import os
+import subprocess
 import sys
 
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
 
 from rbtools import get_package_version, VERSION
 
@@ -86,6 +89,70 @@ with open('README.md') as fp:
     long_description = fp.read()
 
 
+class DevelopCommand(develop):
+    """Installs RBTools in developer mode.
+
+    This will install all standard and development dependencies (using Python
+    wheels) and add the source tree to the Python module search path. That
+    includes updating the versions of pip and setuptools on the system.
+    """
+
+    user_options = develop.user_options + [
+        (str('with-doc-deps'), None,
+         'Install documentation-related dependencies'),
+    ]
+
+    boolean_options = develop.boolean_options + [
+        str('with-doc-deps'),
+    ]
+
+    def initialize_options(self):
+        """Initialize options for the command."""
+        develop.initialize_options(self)
+
+        self.with_doc_deps = None
+
+    def install_for_development(self):
+        """Install the package for development.
+
+        This takes care of the work of installing all dependencies.
+        """
+        if self.no_deps:
+            # In this case, we don't want to install any of the dependencies
+            # below. However, it's really unlikely that a user is going to
+            # want to pass --no-deps.
+            #
+            # Instead, what this really does is give us a way to know we've
+            # been called by `pip install -e .`. That will call us with
+            # --no-deps, as it's going to actually handle all dependency
+            # installation, rather than having easy_install do it.
+            develop.install_for_development(self)
+            return
+
+        self._run_pip(['install', '-e', '.'])
+        self._run_pip(['install', '-r', 'dev-requirements.txt'])
+
+        if self.with_doc_deps:
+            self._run_pip(['install', '-r', 'doc-requirements.txt'])
+
+    def _run_pip(self, args):
+        """Run pip.
+
+        Args:
+            args (list):
+                Arguments to pass to :command:`pip`.
+
+        Raises:
+            RuntimeError:
+                The :command:`pip` command returned a non-zero exit code.
+        """
+        cmd = subprocess.list2cmdline([sys.executable, '-m', 'pip'] + args)
+        ret = os.system(cmd)
+
+        if ret != 0:
+            raise RuntimeError('Failed to run `%s`' % cmd)
+
+
 setup(
     name=PACKAGE_NAME,
     version=get_package_version(),
@@ -118,6 +185,9 @@ setup(
     url='https://www.reviewboard.org/downloads/rbtools/',
     download_url=('https://downloads.reviewboard.org/releases/%s/%s.%s/'
                   % (PACKAGE_NAME, VERSION[0], VERSION[1])),
+    cmdclass={
+        'develop': DevelopCommand,
+    },
     python_requires='>=3.7',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
