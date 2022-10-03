7725165e0e1270582559cbfff7d7b1780ea0ee8e
Christian Hammond <christian@beanbaginc.com>
2022-10-03 14:50:27 -0700

    Release RBTools 3.1.2.

--- rbtools/__init__.py
+++ rbtools/__init__.py
@@ -34,7 +34,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (3, 1, 2, 0, 'alpha', 0, False)
+VERSION = (3, 1, 2, 0, 'final', 0, True)
 
 
 def get_version_string():
