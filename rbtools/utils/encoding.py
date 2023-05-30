e0fe507083f5ae21a3c3e8bc8404a2c6eff8988d
Christian Hammond <christian@beanbaginc.com>
2023-05-30 15:24:34 -0700

    Update force_bytes and force_unicode to support casting objects to strings.
    `force_bytes()` and `force_unicode()` now take a `strings_only` boolean
    argument, which defaults to `True` for backwards-compatibility. When set
    to `False`, these functions can take any arbitrary object and try to
    turn it into a string.
    
    `force_bytes()` will look for a `__bytes__()` dunder method, using that to
    get the result, and then falling back on `__str__()`.
    
    `force_unicode()` will just look for `__str__()`. Since every object has
    this, it will technically work with any object.
    
    Typing has been updated to provide overloads based on the `strings_only`
    parameter, helping to ensure callers receive type warnings if they
    provide something other than a string without opting into
    `strings_only=False`.
    
    Unit tests have been added to cover this module.
    
    Testing Done:
    Unit tests pass.
    
    Tested this with some upcoming fixes for command output rendering.
    
    Reviewed at https://reviews.reviewboard.org/r/13033/

--- rbtools/utils/encoding.py
+++ rbtools/utils/encoding.py
@@ -1,17 +1,63 @@
 """Utilities for managing string types and encoding."""
 
-from typing import Union
+from __future__ import annotations
+
+from typing import Any, Union, overload
+
+from typing_extensions import Literal
+
+
+@overload
+def force_bytes(
+    string: Union[bytes, str],
+    encoding: str = ...,
+    *,
+    strings_only: Literal[True] = ...,
+) -> bytes:
+    ...
+
+
+@overload
+def force_bytes(
+    string: Any,
+    encoding: str = ...,
+    *,
+    strings_only: Literal[False],
+) -> bytes:
+    ...
 
 
 def force_bytes(
-    string: Union[bytes, str],
+    string: Any,
     encoding: str = 'utf-8',
+    *,
+    strings_only: bool = True,
 ) -> bytes:
-    """Force a given string to be a bytes type.
+    """Force a given string to be a byte string type.
+
+    Version Changed:
+        5.0:
+        Added the ``strings_only`` parameter.
 
     Args:
-        string (bytes or str):
-            The string to enforce.
+        string (bytes or str or object):
+            The string to enforce, or an object that can cast to a string
+            type.
+
+        encoding (str, optional):
+            The optional encoding, if encoding Unicode strings.
+
+        strings_only (bool, optional):
+            Whether to only transform byte/Unicode strings.
+
+            If ``True`` (the default), any other type will result in an error.
+
+            If ``False``, the object will be cast to a string using
+            the object's :py:meth:`~object.__bytes__` or
+            :py:meth:`~object.__str__` method.
+
+            Version Added:
+                5.0
 
     Returns:
         bytes:
@@ -25,19 +71,67 @@ def force_bytes(
         return string
     elif isinstance(string, str):
         return string.encode(encoding)
-    else:
-        raise ValueError('Provided string was neither bytes nor unicode')
+    elif not strings_only:
+        if hasattr(string, '__bytes__'):
+            return bytes(string)
+        else:
+            return str(string).encode(encoding)
+
+    raise ValueError(
+        'The provided value could not be cast to a byte string: %r'
+        % (string,))
+
+
+@overload
+def force_unicode(
+    string: Union[bytes, str],
+    encoding: str = ...,
+    *,
+    strings_only: Literal[True] = ...,
+) -> str:
+    ...
+
+
+@overload
+def force_unicode(
+    string: Any,
+    encoding: str = ...,
+    *,
+    strings_only: Literal[False],
+) -> str:
+    ...
 
 
 def force_unicode(
-    string: Union[bytes, str],
+    string: Any,
     encoding: str = 'utf-8',
+    *,
+    strings_only: bool = True,
 ) -> str:
     """Force a given string to be a Unicode string type.
 
+    Version Changed:
+        5.0:
+        Added the ``strings_only`` parameter.
+
     Args:
-        string (bytes or str):
-            The string to enforce.
+        string (bytes or str or object):
+            The string to enforce, or an object that can cast to a string
+            type.
+
+        encoding (str, optional):
+            The optional encoding, if decoding byte strings.
+
+        strings_only (bool, optional):
+            Whether to only transform byte/Unicode strings.
+
+            If ``True`` (the default), any other type will result in an error.
+
+            If ``False``, the object will be cast to a string using
+            the object's :py:meth:`~object.__str__` method.
+
+            Version Added:
+                5.0
 
     Returns:
         str:
@@ -51,5 +145,9 @@ def force_unicode(
         return string
     elif isinstance(string, bytes):
         return string.decode(encoding)
+    elif not strings_only and hasattr(string, '__str__'):
+        return str(string)
     else:
-        raise ValueError('Provided string was neither bytes nor unicode')
+        raise ValueError(
+            'The provided value could not be cast to a Unicode string: %r'
+            % (string,))
