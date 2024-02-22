aa1eede091fb029d85ea5ce4cc362e63b4ce8f4f
David Trowbridge <trowbrds@gmail.com>
2024-02-21 18:44:30 -0700

    Add a function for getting the value of a server capability.
    The capabilities returned by the server have traditionally been
    booleans, but we're adding some more of other types (such as a list of
    supported MIME types for review, and potentially some file size limits).
    This change adds a new method to return the content of a capability
    instead of just whether it's equal to `True`.
    
    Testing Done:
    Used from another change.
    
    Reviewed at https://reviews.reviewboard.org/r/13568/

--- rbtools/api/capabilities.py
+++ rbtools/api/capabilities.py
@@ -1,6 +1,6 @@
 """Review Board server capability management."""
 
-from typing import Mapping
+from typing import Any, Mapping
 
 
 class Capabilities:
@@ -37,17 +37,34 @@ class Capabilities:
             if the capability is set to ``False`` or is not present in the API
             capabilities.
         """
+        # If only part of a capability path is specified, we don't want to
+        # evaluate to True just because it has contents. We want to only
+        # say we have a capability if it is indeed 'True'.
+        return self.get_capability(*cap_path) is True
+
+    def get_capability(self, *cap_path) -> Any:
+        """Return the capability at the given path.
+
+        Version Added:
+            5.0
+
+        Args:
+            *cap_path (tuple of str):
+                The keys forming a path to the capability.
+
+        Returns:
+            Any:
+            The given capability setting. If not present, this will return
+            ``None``.
+        """
         caps = self.capabilities
 
         try:
             for key in cap_path:
                 caps = caps[key]
 
-            # If only part of a capability path is specified, we don't want to
-            # evaluate to True just because it has contents. We want to only
-            # say we have a capability if it is indeed 'True'.
-            return caps is True
+            return caps
         except (TypeError, KeyError):
             # The server either doesn't support the capability, or returned no
             # capabilities at all.
-            return False
+            return None
