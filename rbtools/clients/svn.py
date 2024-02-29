14a465f9148fb2855f26b955411e438cd1d84221
David Trowbridge <trowbrds@gmail.com>
2024-02-29 11:11:14 -0700

    Remove deprecated find_server_repository_info methods.
    We used to have a huge hack where RBTools would do a bunch of stuff to
    try to find the local repository, but then the client could replace the
    entire repository info structure. In 3.0 we cleaned all this up to add
    the `update_from_remote` method, which would let the client fetch
    whatever data it needed. When we did this we kept the old methods around
    with a deprecation warning because third party tools and scripts could
    potentially still be calling them. This change finally removes the
    methods.
    
    Testing Done:
    Ran unit tests.
    
    Reviewed at https://reviews.reviewboard.org/r/13595/

--- rbtools/clients/svn.py
+++ rbtools/clients/svn.py
@@ -16,9 +16,7 @@ from urllib.parse import unquote
 from housekeeping import deprecate_non_keyword_only_args
 
 from rbtools.api.errors import APIError
-from rbtools.api.resource import (ItemResource,
-                                  ListResource,
-                                  RootResource)
+from rbtools.api.resource import ListResource
 from rbtools.clients import PatchResult, RepositoryInfo
 from rbtools.clients.base.scmclient import (BaseSCMClient,
                                             SCMClientDiffResult,
@@ -30,8 +28,7 @@ from rbtools.clients.errors import (AuthenticationError,
                                     SCMClientDependencyError,
                                     SCMError,
                                     TooManyRevisionsError)
-from rbtools.deprecation import (RemovedInRBTools40Warning,
-                                 RemovedInRBTools50Warning)
+from rbtools.deprecation import RemovedInRBTools50Warning
 from rbtools.diffs.writers import UnifiedDiffWriter
 from rbtools.utils.checks import check_install
 from rbtools.utils.console import get_pass
@@ -42,7 +39,6 @@ from rbtools.utils.filesystem import (make_empty_files, make_tempfile,
 from rbtools.utils.process import (RunProcessError,
                                    RunProcessResult,
                                    run_process)
-from rbtools.utils.repository import get_repository_resource
 from rbtools.utils.streams import BufferedIterator
 
 
@@ -1635,49 +1631,6 @@ class SVNRepositoryInfo(RepositoryInfo):
             self.path = url
             self.base_path = relpath
             self.repository_id = cast(int, repository.id)
-
-    def find_server_repository_info(
-        self,
-        api_root: RootResource,
-    ) -> SVNRepositoryInfo:
-        """Return server-side information on the current Subversion repository.
-
-        The point of this function is to find a repository on the server that
-        matches self, even if the paths aren't the same. (For example, if self
-        uses an 'http' path, but the server uses a 'file' path for the same
-        repository.) It does this by comparing repository UUIDs. If the
-        repositories use the same path, you'll get back self, otherwise you'll
-        get a different SVNRepositoryInfo object (with a different path).
-
-        Deprecated:
-            3.0:
-            Commands which need to use the remote repository, or need data from
-            the remote repository such as the base path, should set
-            :py:attr:`needs_repository`.
-
-        Args:
-            api_root (rbtools.api.resource.RootResource):
-                The root resource for the Review Board server.
-
-        Returns:
-            SVNRepositoryInfo:
-            The server-side information for this repository.
-        """
-        RemovedInRBTools40Warning.warn(
-            'The find_server_repository_info method is deprecated, and will '
-            'be removed in RBTools 4.0. If you need to access the remote '
-            'repository, set the needs_repository attribute on your Command '
-            'subclass.')
-
-        repository, info = get_repository_resource(
-            api_root,
-            tool=self.tool,
-            repository_paths=self.path)
-
-        if repository:
-            self.update_from_remote(repository, info)
-
-        return self
 
     def _get_relative_path(
         self,
