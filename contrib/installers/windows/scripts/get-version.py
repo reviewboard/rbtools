0a938f7abc5081b71e2b9f14d0a9328cfa9f9ce0
Christian Hammond <christian@beanbaginc.com>
2024-12-02 21:44:58 -0800

    Add additional logging and protection for the Windows installer.
    The Windows Installer hit some snags in today's release. To help debug
    this, we've added extra logging to the installer building phases,
    additional error handling, and more explicit return values when exiting
    subroutines.

--- contrib/installers/windows/scripts/get-version.py
+++ contrib/installers/windows/scripts/get-version.py
@@ -1,9 +1,14 @@
-from rbtools import VERSION
+import sys
 
+try:
+    from rbtools import VERSION
 
-# MSI files only use the first 3 version fields, and has no concept of
-# alphas/betas/RCs/patch levels.
-if VERSION[2] == 0:
-    print('%s.%s' % VERSION[:2])
-else:
-    print('%s.%s.%s' % VERSION[:3])
+    # MSI files only use the first 3 version fields, and has no concept of
+    # alphas/betas/RCs/patch levels.
+    if VERSION[2] == 0:
+        print('%s.%s' % VERSION[:2])
+    else:
+        print('%s.%s.%s' % VERSION[:3])
+except Exception as e:
+    print('Failed to determine version: %s' % e)
+    sys.exit(1)
